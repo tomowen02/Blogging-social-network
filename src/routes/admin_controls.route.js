611263a650b0f4cb2ecc3beef910f52502943aa6
tomowen02 <tom@tomiowen.com>
2023-05-27 19:16:00 +0100

    Create admin controls pages

--- src/routes/admin_controls.route.js
+++ src/routes/admin_controls.route.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render('admin_controls/test')
+    res.render('admin_controls/admin_controls');
+});
+router.get('/categories', async (req, res) => {
+    res.render('admin_controls/admin_controls', { subpage: "categories" });
 });
 
 module.exports = router;
